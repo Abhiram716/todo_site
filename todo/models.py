4a272a1e2e5f11b9334c28eaf1253496d89a739f
Abhiram <a4abhiram125@gmail.com>
2023-05-31 11:09:20 +0530

    created a taskstatus feild

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.utils import timezone
  
- 
+TASK_STATUS = (
+	("OPEN","OPEN"),
+	("WORKING","WORKING"),
+	("DONE","DONE"),
+	("OVERDUE","OVERDUE"),
+)
+
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField()
     date = models.DateTimeField(default=timezone.now)
- 
+    taskstatus = models.CharField(
+        max_length=20,
+	    choices = TASK_STATUS,
+	    default = 'OPEN'
+    )
+
     def __str__(self):
         return self.title	
